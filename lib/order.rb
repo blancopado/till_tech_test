142a74621d43204591806ab8ca9f32c03d3ee0a6
blancopado <pablo.dominguezblanco@gmail.com>
2016-01-14 12:58:28 +0100

    adding and item and all its related methods

--- lib/order.rb
+++ lib/order.rb
@@ -7,15 +7,34 @@ class Order
 		@basket = []
 		@total_amount = 0
 		file = File.read('../hipstercoffee.json')
-		data_hash = JSON.parse(file)
-		#p data_hash
-		#p data_hash[0]["address"]
+		@data_hash = JSON.parse(file.downcase)
+		@prices = @data_hash[0]["prices"][0]
+		#p @data_hash
 	end
 
-	def add
-
+	def add(item, quantity = 1)
+		@item = item.downcase
+  	@quantity = quantity
+  	available ? update_basket : not_update_basket
 	end
+
+	def available
+		@prices.include?(@item)
+	end
+
+	def update_basket
+		@basket << { name: @item, quantity: @quantity, 
+  	  	        price: @prices[@item],
+  	  	        accumulated_prize: accumulated_prize
+  	  	        }
+	end
+
+	def not_update_basket
+		"We don't have it in the menu."
+	end
+
+	def accumulated_prize
+  	@prices[@item] * @quantity
+  end
 
 end
-
-order = Order.new "pablo"
